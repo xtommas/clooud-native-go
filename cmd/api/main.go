c54e7817b2ebc9cbaa19b1dff879ce81d8fa4633
Tomas <tomascanepa1397@gmail.com>
2024-03-11 18:41:51 -0300

    Hello world server, Database and migrations, Configurations

---
+++ cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"myapp/config"
+)
+
+func main() {
+	c := config.New()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", hello)
+
+	s := &http.Server{
+		Addr:         fmt.Sprintf(":%d", c.Server.Port),
+		Handler:      mux,
+		ReadTimeout:  c.Server.TimeoutRead,
+		WriteTimeout: c.Server.TimeoutWrite,
+		IdleTimeout:  c.Server.TimeoutIdle,
+	}
+
+	log.Println("Starting server " + s.Addr)
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal("Server startup failed")
+	}
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!")
+}
